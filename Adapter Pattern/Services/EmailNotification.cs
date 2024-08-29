f975361901c5ceed8b5ad7dc312734036a9d6099
HMErni <hero.misa@betterask.erni>
2024-08-29 18:33:45 +0800

    Modify Adapter

--- Adapter Pattern/Services/EmailNotification.cs
+++ Adapter Pattern/Services/EmailNotification.cs
@@ -7,6 +7,7 @@ public class EmailNotification
     public void SendEmail(string recipient, string subject, string message)
     {
         Console.WriteLine($"To: {recipient}");
+        Console.WriteLine($"Subject: {subject}");
         Console.WriteLine($"Message: {message}");
         Console.WriteLine("Sent via Email");
     }
